bc54a8fe33b30b359705c5d451ed0c02f7fb44ea
Ping Xiong <p.xiong@f5.com>
2022-04-27 13:53:41 +0800

    update path.

--- etcd/etcd_exec_watch_bigip.sh
+++ etcd/etcd_exec_watch_bigip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 HOST_IP=$(ip -f inet a show ens38 | grep -oP "(?<=inet ).+(?=\/)")
-etcdctl --no-sync --endpoint http://$HOST_IP:4001 exec-watch --recursive / -- sh -c /root/NGINX-Demos/etcd-demo/script-bigip.sh;
+etcdctl --no-sync --endpoint http://$HOST_IP:4001 exec-watch --recursive / -- sh -c ~/MSDA-Demo/etcd/script-bigip.sh;
