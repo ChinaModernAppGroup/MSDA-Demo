402ebc30e269c63c37fbb7108d7e6993eccfed80
xp2014 <p.xiong@f5.com>
2022-09-18 18:18:39 +0800

    update watch script.

--- nacos/watchInstances.sh
+++ nacos/watchInstances.sh
@@ -4,7 +4,10 @@ if [[ -z "$NACOS_IP" ]]; then
   NACOS_IP=127.0.0.1
 fi
 
-nacosAPI="http://$NACOS_IP:8848/nacos/v1/ns/instance/list?serviceName=msda.nacos.com"
+accessToken=$(curl -s -X POST $nacos_login -d 'username=nacos&password=nacos' | jq -c '.accessToken' | sed 's/\"//g')
+#echo $accessToken
+
+nacosAPI="http://$NACOS_IP:8848/nacos/v1/ns/instance/list?serviceName=msda.nacos.com&accessToken="
 
 curl -X GET -s $nacosAPI | jq . | grep "instanceId\":"
 
